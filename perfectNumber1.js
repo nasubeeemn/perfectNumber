ce657550389bf6a9b4f8fbf2bff70570f4e8c669
nasubeeemn <tksfj17@gmail.com>
2017-12-08 12:18:27 +0000

    [perfectNumber1.js]を追加

---
+++ perfectNumber1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * perfectNumber1
+ * @param {number} maxNumber この数までの完全数を計算する
+ */
+function perfectNumber1(maxNumber){
+  for(let i = 1; i <= maxNumber; i++){ // i:割られる数
+    let sum = 0;
+    for(let j = 1; j < i; j++){        // j: 割る数
+      if(i % j === 0){
+        sum = sum + j;
+      }
+    }
+    if(sum === i){
+      console.log(i + ' is perfect number.');
+    }
+  }
+}
+
+perfectNumber1(10);
